6b665f2cc1f8ae762a880a19e949f917e10a003e
waldls <p353700@gmail.com>
2025-08-05 17:46:31 +0900

    [REFACTOR] Footer 색상 변경

--- week8/mission2/src/components/Footer.tsx
+++ week8/mission2/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 dark:bg-gray-900 py-6 mt-12">
-      <div className="container mx-auto text-center text-gray-600 dark:text-gray-400">
+    <footer className="bg-black dark:bg-gray-100 py-6 mt-12">
+      <div className="container mx-auto text-center text-pink-600 dark:text-black">
         <p>
           &copy; {new Date().getFullYear()} SpinningSpinning Dolimpan.All rights
           reserved.
